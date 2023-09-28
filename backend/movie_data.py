b816dfd8e9ef06df0cc8ceae0e6b4431fd766a56
Khalid Omer <komer301@gmail.com>
2023-09-27 22:52:18 -0400

    working on card

--- backend/movie_data.py
+++ backend/movie_data.py
@@ -1,15 +1,16 @@
 import requests
 import json
 
+headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxY2QxMTM2MGQyNmQ1MzlhOTFmYjI2YjllNWViODlkNCIsInN1YiI6IjY1MGU0YWY1ZTFmYWVkMDBlM2Y1MWY3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JcBTbOQwPdpT4MCfmbYH14f9fqWXfIXPFomUp5OyuHE"
+    }   
 
 def multiSearch(title):
     x = title.split()
     final = "%20".join(x)
     url = f"https://api.themoviedb.org/3/search/multi?query={final}&include_adult=true&language=en&page=1"
-    headers = {
-    "accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxY2QxMTM2MGQyNmQ1MzlhOTFmYjI2YjllNWViODlkNCIsInN1YiI6IjY1MGU0YWY1ZTFmYWVkMDBlM2Y1MWY3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JcBTbOQwPdpT4MCfmbYH14f9fqWXfIXPFomUp5OyuHE"
-    }   
+  
 
     response = requests.get(url, headers=headers)
     data = json.loads(response.text)
@@ -21,14 +22,12 @@ def multiSearch(title):
     print(json_formatted2)
 
 def trendingInfo(time_window):
-    
     url = f"https://api.themoviedb.org/3/trending/movie/{time_window}?language=en-US"
 
     headers = {
         "accept": "application/json",
         "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxY2QxMTM2MGQyNmQ1MzlhOTFmYjI2YjllNWViODlkNCIsInN1YiI6IjY1MGU0YWY1ZTFmYWVkMDBlM2Y1MWY3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JcBTbOQwPdpT4MCfmbYH14f9fqWXfIXPFomUp5OyuHE"
     }
-
     response = requests.get(url, headers=headers)
     data = json.loads(response.text)
     if 'results' in data and data['results']:

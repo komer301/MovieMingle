4acf5cf49fa0c704c93528aa51f27a203b1b9f37
Khalid Omer <komer301@gmail.com>
2023-10-01 21:59:54 -0400

    tv shows completed

--- backend/movie_data.py
+++ backend/movie_data.py
@@ -1,10 +1,17 @@
 import requests
+from datetime import date
 import json
 
 headers = {
     "accept": "application/json",
     "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxY2QxMTM2MGQyNmQ1MzlhOTFmYjI2YjllNWViODlkNCIsInN1YiI6IjY1MGU0YWY1ZTFmYWVkMDBlM2Y1MWY3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JcBTbOQwPdpT4MCfmbYH14f9fqWXfIXPFomUp5OyuHE"
     }   
+
+def dateVerify(initialDate):
+    year, month, day = map(int, initialDate.split('-'))
+    today = date.today()
+    input_date = date(year, month, day)
+    return input_date > today
 
 def genreTranslator(id,type):
     if type == "movie":
@@ -25,18 +32,16 @@ def genreTranslator(id,type):
                 return item['name']
 
 
-
 def multiSearch(title):
     x = title.split()
     final = "%20".join(x)
     url = f"https://api.themoviedb.org/3/search/multi?query={final}&include_adult=true&language=en&page=1"
     response = requests.get(url, headers=headers)
     data = json.loads(response.text)
-    for show in data['results']:
-        print(show['id'])
+    # for show in data['results']:
+        # print(show['id'])
     # json_formatted = json.dumps(data['results'], indent=2)
     json_formatted2 = json.dumps(data['total_results'], indent=2)
-    # print(json_formatted)
     print(json_formatted2)
 
 def truncate_movie_title(title, max_chars=28):
@@ -46,35 +51,61 @@ def truncate_movie_title(title, max_chars=28):
         truncated_title = title
     return truncated_title
 
-def trendingInfo(time_window):
-    url = f"https://api.themoviedb.org/3/trending/movie/{time_window}?language=en-US"
+def trendingInfo(type,time_window):
+    url = f"https://api.themoviedb.org/3/trending/{type}/{time_window}?language=en-US"
     response = requests.get(url, headers=headers)
     data = json.loads(response.text)
+    final = []
     if 'results' in data and data['results']:
         for result in data['results']:
             id = result.get('id',0)
             title = truncate_movie_title(result.get('title'))
             genres = result.get('genre_ids')
             for x in range(len(genres)):
-                genres[x] = genreTranslator(genres[x],"movie")
+                genres[x] =  genreTranslator(genres[x],"movie")
             rating = result.get('vote_average',0)
             poster_path = 'https://image.tmdb.org/t/p/w500' + result.get('poster_path','None')
-            print([id,title,genres,rating,poster_path])
+            final.append([id,title,genres,rating,poster_path])
+    return final
 
+def upcomingInfo(type):
+    if type == "movie":
+        url = f"https://api.themoviedb.org/3/{type}/upcoming?language=en-US&page=1"
+    else:
+        url = "https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1"
+    response = requests.get(url, headers=headers)
+    data = json.loads(response.text)
+    final = []
+    if 'results' in data and data['results']:
+        for result in data['results']:
+            release = result.get('release_date')
+            if dateVerify(release):
+                id = result.get('id',0)
+                title = truncate_movie_title(result.get('title'))
+                genres = result.get('genre_ids')
+                for x in range(len(genres)):
+                    genres[x] = genreTranslator(genres[x],"movie")
+                rating = result.get('vote_average',0)
+                
+                poster_path = 'https://image.tmdb.org/t/p/w500' + result.get('poster_path','None')
+                final.append([id,title,genres,rating,poster_path])
+    return final
 
-trendingInfo("week")
+def tvShowTrending():
+    url = "https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1"
+    response = requests.get(url, headers=headers)
+    data = json.loads(response.text)
+    final = []
+    if 'results' in data and data['results']:
+        for result in data['results']:
+            id = result.get('id',0)
+            title = truncate_movie_title(result.get('name'))
+            genres = result.get('genre_ids')
+            for x in range(len(genres)):
+                genres[x] = genreTranslator(genres[x],"tv")
+            rating = result.get('vote_average',0)
+            poster_path = 'https://image.tmdb.org/t/p/w500' + result.get('poster_path','None')
+            final.append([id,title,genres,rating,poster_path])
+    return final
 
-# 38 CHARACTERS MAX
-
-
-# Example usage:
-# movie_title1 = "Creation of the Gods I: Kingdom..."
-# movie_title2 = "Teenage Mutant Ninja Turtles: Mutant Mayhem"
-# truncated_title1 = truncate_movie_title(movie_title1)
-# truncated_title2 = truncate_movie_title(movie_title2)
-# print(truncated_title1)  # Output: "Creation of the Gods I: Kingdom..."
-# print(truncated_title2)  # Output: "The Wonderful Story of Henry..."
-
-# print(genreTranslator(18,"movie"))
-
-
+multiSearch('the office')
--- website/views.py
+++ website/views.py
@@ -1,9 +1,19 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
+from backend import movie_data
 
 views = Blueprint("views",__name__)
 
-@views.route('/')
+@views.route('/movies')
 @login_required
-def home():
-    return render_template("movies.html")
+def movies():
+    data = movie_data.trendingInfo("movie","week")[:8]
+    upcoming = movie_data.upcomingInfo("movie")[:8]
+    return render_template("movies.html", data=data, upcoming=upcoming)
+
+@views.route('/tvshows')
+@login_required
+def tvshows():
+    data = movie_data.tvShowTrending()[:8]
+    # upcoming = movie_data.upcomingInfo("tv")[:8]
+    return render_template("tvshows.html", data=data)
